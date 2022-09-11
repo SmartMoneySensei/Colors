d08b1c1543563846e74ac56d952bf73641d2ffb4
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-11 11:56:38 -0700

    colors

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// Declare routes that people can visit on the application.
+app.get('/', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
+})
+
+// Teal Page
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
+        </body>
+    `)
+})
+
+
+
+app.listen(process.env.PORT, function(){
+    console.log('I am Awake')
+})
